2c7df0ec6b996d0d55c841cc122f43951ce963e3
Kay Altos <kayaltosnexus@gmail.com>
2022-11-21 00:59:07 +0300

    Создал классы для реализации анимаций

---
+++ KARC/WitchEngine/Animations/AnimationAtlas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace KARC.WitchEngine.Animations;
+public class AnimationAtlas
+{
+    public int ImageId { get; }
+    private int _counter;
+    public AnimationFrame[] AnimationFrames { get; }
+
+    public AnimationAtlas (int imageId, int frameCount)
+    {
+        ImageId = imageId;
+        AnimationFrames = new AnimationFrame[frameCount];
+        _counter = 0;
+    }
+    public void AddFrame (AnimationFrame animationFrame)
+    {        
+        if (_counter < AnimationFrames.Length)
+            AnimationFrames[_counter] = animationFrame;
+        else
+            throw new Exception("Too many frames");
+    }
+}
---
+++ KARC/WitchEngine/Animations/Animator.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+namespace KARC.WitchEngine.Animations;
+public class Animator
+{
+    private AnimationAtlas _animationAtlas;
+    private int _frameTime;
+    private int _deltaTime;
+    private int _currentFrameIndex;
+    private bool _isCycled;    
+    public bool IsActive { get; private set; }
+    public AnimationFrame CurrentFrame { get; private set; }
+    public Animator (AnimationAtlas animationAtlas, int frameTime, bool isCycled)
+    {
+        _animationAtlas = animationAtlas;
+        _frameTime = frameTime;
+        _isCycled = isCycled;
+        _deltaTime = 0;
+        _currentFrameIndex = 0;
+        IsActive = false;
+    }
+    public void Activate()
+    {
+        IsActive = true;
+    }
+    public void Update(GameTime gameTime)
+    {
+        if (IsActive == false)
+            return;
+
+        _deltaTime += gameTime.ElapsedGameTime.Milliseconds;
+        if (_deltaTime > _frameTime)
+        {
+            _deltaTime = 0;
+            _currentFrameIndex++;
+            if (_currentFrameIndex >= _animationAtlas.AnimationFrames.Length)
+            {
+                if (_isCycled == true)
+                    _currentFrameIndex = 0;
+                else
+                    IsActive = false;
+            }
+            if (IsActive == true)
+                CurrentFrame = _animationAtlas.AnimationFrames[_currentFrameIndex];
+        }
+    }
+}
